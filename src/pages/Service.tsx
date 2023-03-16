602c4c24751363d7e938a9195f61a7a21fdbb2f1
Angelo Rodriguez <geloor.or@gmail.com>
2023-03-16 16:30:23 +0800

    created service card component - updated service component for shorter coding

--- src/pages/Service.tsx
+++ src/pages/Service.tsx
@@ -1,23 +1,15 @@
-import ButtonStyle from "../components/ButtonStyle";
 import HeaderStyle from "../components/HeaderStyle";
+import ServiceCard from "../components/ServiceCard";
 
 import service_1 from "../assets/images/service-1.png";
 import service_2 from "../assets/images/service-2.png";
 import service_3 from "../assets/images/service-3.png";
 
 export default function Service() {
-  const containerStyle1 =
-    "flex flex-col justify-center gap-10 border-2 border-gray-200 border-solid pb-4 hover:shadow-xl";
-  const containerStyle2 =
-    "flex flex-col items-center justify-center gap-2 px-14 sm:px-4";
-
-  const cutStyle = "font-poppins uppercase text-base text-gray-800 font-bold";
-  const pStyle = "font-poppins text-gray-500 text-xs pb-2";
-
   return (
     <section
       id='service'
-      className='px-12 py-28 bg-gray-100 text-center sm:px-40'
+      className='px-12 py-28 bg-gray-100 text-center font-poppins sm:px-40'
     >
       <HeaderStyle
         color={"gray-400"}
@@ -27,41 +19,13 @@ export default function Service() {
       <HeaderStyle color={"yellow-500"} text={"Our Services"} size={"4xl"} />
 
       <div className='flex flex-col items-center justify-center gap-10 sm:flex-row pt-10'>
-        <section className={containerStyle1}>
-          <img src={service_1} alt='' />
-          <div className={containerStyle2}>
-            <h3 className={cutStyle}>Classic Haircut</h3>
-            <p className={pStyle}>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </p>
-            <ButtonStyle color={"gray-800"} hoverColor={"yellow-500"} />
-          </div>
-        </section>
-
-        <section className={containerStyle1}>
-          <img src={service_2} alt='' />
-          <div className={containerStyle2}>
-            <h3 className={cutStyle}>Clipper Cut</h3>
-            <p className={pStyle}>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </p>
-            <ButtonStyle color={"gray-800"} hoverColor={"yellow-500"} />
-          </div>
-        </section>
-
-        <section className={containerStyle1}>
-          <img src={service_3} alt='' />
-          <div className={containerStyle2}>
-            <h3 className={cutStyle}>Razoe Shaver</h3>
-            <p className={pStyle}>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </p>
-            <ButtonStyle color={"gray-800"} hoverColor={"yellow-500"} />
-          </div>
-        </section>
+        {[
+          [service_1, "Classic Haircut"],
+          [service_2, "Clipper Cut"],
+          [service_3, "Razoe Shaver"],
+        ].map(([url, text]) => (
+          <ServiceCard imageUrl={url} haircut={text} />
+        ))}
       </div>
     </section>
   );
