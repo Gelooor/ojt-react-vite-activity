02b17eb535674cf28820eb7ede1c87520dd7617a
Angelo Rodriguez <geloor.or@gmail.com>
2023-03-16 16:42:34 +0800

    created team card component - updated team component for shorter coding

---
+++ src/components/TeamCard.tsx
@@ -0,0 +1,23 @@
+interface ITeamProps {
+  imageUrl: string;
+  name: string;
+  job: string;
+}
+
+export default function TeamCard({
+  imageUrl,
+  name,
+  job,
+}: ITeamProps): JSX.Element {
+  return (
+    <div className='flex flex-col items-center justify-center gap-10 sm:flex-row pt-10'>
+      <div className='flex flex-col justify-center gap-3 border-2 border-gray-200 border-solid pb-4 hover:shadow-xl'>
+        <img src={imageUrl} alt='' />
+        <div className='flex flex-col items-center justify-center gap-2 px-14 sm:px-4'>
+          <p className='font-bold'>{name}</p>
+          <p>{job}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/Team.tsx
+++ src/pages/Team.tsx
@@ -1,15 +1,11 @@
 import HeaderStyle from "../components/HeaderStyle";
+import TeamCard from "../components/TeamCard";
 
 import team_1 from "../assets/images/team-img1.png";
 import team_2 from "../assets/images/team-img2.png";
 import team_3 from "../assets/images/team-img3.png";
 
 export default function Team() {
-  const containerStyle1 =
-    "flex flex-col justify-center gap-3 border-2 border-gray-200 border-solid pb-4 hover:shadow-xl";
-  const containerStyle2 =
-    "flex flex-col items-center justify-center gap-2 px-14 sm:px-4";
-
   return (
     <section
       id='team'
@@ -19,27 +15,13 @@ export default function Team() {
       <HeaderStyle color={"yellow-500"} text={"Our Team"} size={"4xl"} />
 
       <div className='flex flex-col items-center justify-center gap-10 sm:flex-row pt-10'>
-        <div className={containerStyle1}>
-          <img src={team_1} alt='' />
-          <div className={containerStyle2}>
-            <p className='font-bold'>Paul Narch</p>
-            <p>Barber</p>
-          </div>
-        </div>
-        <div className={containerStyle1}>
-          <img src={team_2} alt='' />
-          <div className={containerStyle2}>
-            <p className='font-bold'>Steven Josephin</p>
-            <p>Barber</p>
-          </div>
-        </div>
-        <div className={containerStyle1}>
-          <img src={team_3} alt='' />
-          <div className={containerStyle2}>
-            <p className='font-bold'>Alexander David</p>
-            <p>Shaver</p>
-          </div>
-        </div>
+        {[
+          [team_1, "Paul Narch", "Barber"],
+          [team_2, "Steven Joseph", "Barber"],
+          [team_3, "Alexander David", "Shaver"],
+        ].map(([url, name, job]) => (
+          <TeamCard imageUrl={url} name={name} job={job} />
+        ))}
       </div>
     </section>
   );
