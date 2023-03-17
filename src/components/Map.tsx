4efe2736ebb4c9417f278e866b6c825f21846f3a
Angelo Rodriguez <geloor.or@gmail.com>
2023-03-17 14:09:05 +0800

    fixed issues

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,19 +1,18 @@
 import { GoogleMap, useLoadScript, MarkerF } from "@react-google-maps/api";
+import { useMemo } from "react";
 
 export default function Map() {
   const {} = useLoadScript({
     googleMapsApiKey: "AIzaSyCMgjbPjsa3zd48dyCHRyOI7GTkoGYBJA8",
   });
 
+  const center = useMemo(() => ({ lat: 14.5868245, lng: 121.0281563 }), []);
+
   // if (isLoaded) return <div>Loading...</div>;
 
   return (
-    <GoogleMap
-      zoom={20}
-      center={{ lat: 14.362406, lng: 121.034894 }}
-      mapContainerClassName='h-28 sm:h-96'
-    >
-      <MarkerF position={{ lat: 14.362406, lng: 121.034894 }} />
+    <GoogleMap zoom={20} center={center} mapContainerClassName='h-28 sm:h-96'>
+      <MarkerF position={center} />
     </GoogleMap>
   );
 }
