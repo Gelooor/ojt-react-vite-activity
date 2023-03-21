c457737deffd5b546ad087aac1e4cf9bdb6a9d83
Angelo Rodriguez <geloor.or@gmail.com>
2023-03-21 10:39:10 +0800

    added global css to use for transparent button in HeroSection

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,6 +1,4 @@
 export default function HeroSection() {
-  const buttonStyle =
-    "font-poppins uppercase w-36 h-10 rounded-3xl border-2 border-solid text-white border-transparent border-white hover:bg-yellow-500 hover:border-yellow-500";
   return (
     <section className='text-white text-left px-12 py-60 flex flex-col gap-2 sm:pl-40'>
       <h1 className='font-crimsonText text-4xl'>
@@ -13,8 +11,8 @@ export default function HeroSection() {
       </p>
 
       <div className='flex flex-col gap-2 pt-2 sm:flex-row'>
-        <button className={buttonStyle}>Learn More</button>
-        <button className={buttonStyle}>Contact Us</button>
+        <button className='transparent-button'>Learn More</button>
+        <button className='transparent-button'>Contact Us</button>
       </div>
     </section>
   );
